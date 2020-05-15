addddfab13692c6fb6ed217576e269e2168db514
Ned <6065203@RTS-365.dk>
2020-05-15 12:19:06 +0200

    pretty url done med tilføjet tasks til gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
 
-const gulp = require('gulp');
-const ejs = require('gulp-ejs');
-const rename = require('gulp-rename');
 const connect = require('gulp-connect');
+const { watchHTML, buildHTML } = require('./tasks/html');
+const { watchSCSS, buildSCSS } = require('./tasks/scss');
+// const { watchJS, buildJS } = require('./tasks/js');
+const { watchIMG, buildIMG } = require('./tasks/images');
+const { watchMEDIA, buildMEDIA } = require('./tasks/media');
 
 
-
-function html() {
-    return gulp
-        .src("./src/html/templates/*ejs")
-        .pipe(ejs())
-        .pipe(rename(function (path) {
-            path.extname = ".html"  
-        }))
-        .pipe(gulp.dest("./tmp"))
-        .pipe(connect.reload());
-}
-
-function watchHTML() {
-    return gulp
-        .watch("./src/html/**/*.ejs", {
-            ignoreInitial: false
-        }, html);
-}
-
-gulp.task("dev", function (done){
+function dev(done) {
     watchHTML();
+    watchSCSS();
+    // watchJS();
+    watchIMG();
+    watchMEDIA();
     connect.server({
         livereload: true,
+        port: 3000,
         root: "tmp"
     })
-    done();
-})
+    done()
+}
+
+function build(done) {
+    buildHTML();
+    buildSCSS();
+    // buildJS();
+    buildIMG();
+    buildMEDIA();
+    done()
+}
+
+exports.default = dev;
+exports.build = build;
