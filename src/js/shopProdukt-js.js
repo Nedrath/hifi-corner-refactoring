3265de38f7de081b74cf88b0a6c787cac13c6250
Ned <6065203@RTS-365.dk>
2020-05-20 11:30:00 +0200

    lavet rettelser på diverse links og js til shopprodukt

--- src/js/shopProdukt-js.js
+++ src/js/shopProdukt-js.js
@@ -1,33 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    fetch("https://hifi-corner.herokuapp.com/api/v1/products", {
+    let searchParams = new URLSearchParams(document.location.search);
+    let sku = (searchParams.get("id"));
+
+    fetch(`https://hifi-corner.herokuapp.com/api/v1/products/${sku}`, {
         "method": "GET"
     })
-        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            return response.json()
+        } )
         .then(data => {
 
-            let searchParams = new URLSearchParams(document.location.search);
-            let paramsId = parseInt(searchParams.get("id"));
-            let current = data.find(product => product.sku == paramsId)
+
 
             // PRODUKT BILLEDE SECTION
             let billedeSection = document.querySelector(".shop-produkt__billede")
-            billedeSection.setAttribute("data-id", current.sku)
-            billedeSection.id = 'p' + current.sku
+            billedeSection.setAttribute("data-id", data.sku)
+            billedeSection.id = 'p' + data.sku
 
             billedeSection.innerHTML = `
-                <img src="${current.images}" alt="">
+                <img src="${data.images}" alt="">
                 
                 `;
 
             // THUMBNAIL SECTION
             // let thumbnailSection = document.querySelector(".shop-produkt__thumbnail")
-            // thumbnailSection.setAttribute("data-id", current.sku)
-            // thumbnailSection.id = 'p' + current.sku
+            // thumbnailSection.setAttribute("data-id", data.sku)
+            // thumbnailSection.id = 'p' + data.sku
 
-            // if (current.more_views.length) {
+            // if (data.more_views.length) {
 
-            //     current.more_views.forEach(element => {
+            //     data.more_views.forEach(element => {
 
             //         thumbnailSection.innerHTML += `
             //         <img class="thumbnail__small-pictures" src="/images/Produktbilleder/${element}" alt="">
@@ -37,122 +41,122 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // DESCRIPTION SECTION
             // let powerSection = document.querySelector(".specifikation__power")
-            // powerSection.setAttribute("data-id", current.id)
-            // powerSection.id = 'p' + current.id
-            // powerSection.innerText = current.description["Power-Output"]
+            // powerSection.setAttribute("data-id", data.id)
+            // powerSection.id = 'p' + data.id
+            // powerSection.innerText = data.description["Power-Output"]
 
             // let frequencySection = document.querySelector(".specifikation__frequency")
-            // frequencySection.setAttribute("data-id", current.id)
-            // frequencySection.id = 'p' + current.id
-            // frequencySection.innerText = current.description["Frequency-Response"]
+            // frequencySection.setAttribute("data-id", data.id)
+            // frequencySection.id = 'p' + data.id
+            // frequencySection.innerText = data.description["Frequency-Response"]
 
             // let totalSection = document.querySelector(".specifikation__total")
-            // totalSection.setAttribute("data-id", current.id)
-            // totalSection.id = 'p' + current.id
-            // totalSection.innerText = current.description["Total-Harmonic-Distortion"]
+            // totalSection.setAttribute("data-id", data.id)
+            // totalSection.id = 'p' + data.id
+            // totalSection.innerText = data.description["Total-Harmonic-Distortion"]
 
             // let dampingSection = document.querySelector(".specifikation__damping")
-            // dampingSection.setAttribute("data-id", current.id)
-            // dampingSection.id = 'p' + current.id
-            // dampingSection.innerText = current.description["Damping-Factor"]
+            // dampingSection.setAttribute("data-id", data.id)
+            // dampingSection.id = 'p' + data.id
+            // dampingSection.innerText = data.description["Damping-Factor"]
 
             // let inputMMSection = document.querySelector(".specifikation__inputMM")
-            // inputMMSection.setAttribute("data-id", current.id)
-            // inputMMSection.id = 'p' + current.id
-            // inputMMSection.innerText = current.description["Input-Sensitivity:MM"]
+            // inputMMSection.setAttribute("data-id", data.id)
+            // inputMMSection.id = 'p' + data.id
+            // inputMMSection.innerText = data.description["Input-Sensitivity:MM"]
 
             // let inputMCSection = document.querySelector(".specifikation__inputMC")
-            // inputMCSection.setAttribute("data-id", current.id)
-            // inputMCSection.id = 'p' + current.id
-            // inputMCSection.innerText = current.description["Input-Sensitivity:MC"]
+            // inputMCSection.setAttribute("data-id", data.id)
+            // inputMCSection.id = 'p' + data.id
+            // inputMCSection.innerText = data.description["Input-Sensitivity:MC"]
 
             // let signalSection = document.querySelector(".specifikation__signal")
-            // signalSection.setAttribute("data-id", current.id)
-            // signalSection.id = 'p' + current.id
-            // signalSection.innerText = current.description["Signal-to-Noise-Ratio: MM / MC"]
+            // signalSection.setAttribute("data-id", data.id)
+            // signalSection.id = 'p' + data.id
+            // signalSection.innerText = data.description["Signal-to-Noise-Ratio: MM / MC"]
 
             // let input1Section = document.querySelector(".specifikation__input1")
-            // input1Section.setAttribute("data-id", current.id)
-            // input1Section.id = 'p' + current.id
-            // input1Section.innerText = current.description["Input-Sensitivity: High level"]
+            // input1Section.setAttribute("data-id", data.id)
+            // input1Section.id = 'p' + data.id
+            // input1Section.innerText = data.description["Input-Sensitivity: High level"]
 
             // let inputBalancedSection = document.querySelector(".specifikation__inputBalanced")
-            // inputBalancedSection.setAttribute("data-id", current.id)
-            // inputBalancedSection.id = 'p' + current.id
-            // inputBalancedSection.innerText = current.description["Input-Sensitivity: Balanced High Level"]
+            // inputBalancedSection.setAttribute("data-id", data.id)
+            // inputBalancedSection.id = 'p' + data.id
+            // inputBalancedSection.innerText = data.description["Input-Sensitivity: Balanced High Level"]
 
             // let signalHighSection = document.querySelector(".specifikation__signalHigh")
-            // signalHighSection.setAttribute("data-id", current.id)
-            // signalHighSection.id = 'p' + current.id
-            // signalHighSection.innerText = current.description["Signal-to-Noise-Ratio: High level"]
+            // signalHighSection.setAttribute("data-id", data.id)
+            // signalHighSection.id = 'p' + data.id
+            // signalHighSection.innerText = data.description["Signal-to-Noise-Ratio: High level"]
 
             // let inputPowerSection = document.querySelector(".specifikation__inputPower")
-            // inputPowerSection.setAttribute("data-id", current.id)
-            // inputPowerSection.id = 'p' + current.id
-            // inputPowerSection.innerText = current.description["Input-Sensitivity: Power Amp Direct IN"]
+            // inputPowerSection.setAttribute("data-id", data.id)
+            // inputPowerSection.id = 'p' + data.id
+            // inputPowerSection.innerText = data.description["Input-Sensitivity: Power Amp Direct IN"]
 
             // let signalPowerSection = document.querySelector(".specifikation__signalPower")
-            // signalPowerSection.setAttribute("data-id", current.id)
-            // signalPowerSection.id = 'p' + current.id
-            // signalPowerSection.innerText = current.description["Signal-to-Noise-Ratio: Power Amp Direct IN"]
+            // signalPowerSection.setAttribute("data-id", data.id)
+            // signalPowerSection.id = 'p' + data.id
+            // signalPowerSection.innerText = data.description["Signal-to-Noise-Ratio: Power Amp Direct IN"]
 
             // PRODUKT INFO SECTION
             let produktOverskrift = document.querySelector(".info__overskrift")
-            produktOverskrift.setAttribute("data-id", current.sku)
-            produktOverskrift.id = 'p' + current.sku
-            produktOverskrift.innerHTML = `${current.make}  ${current.model}`;
+            produktOverskrift.setAttribute("data-id", data.sku)
+            produktOverskrift.id = 'p' + data.sku
+            produktOverskrift.innerHTML = `${data.make}  ${data.model}`;
 
             let productBrand = document.querySelector(".info__otherProducts_brand")
-            productBrand.setAttribute("data-id", current.sku)
-            productBrand.id = 'p' + current.sku
-            productBrand.innerHTML = `${current.make} `;
+            productBrand.setAttribute("data-id", data.sku)
+            productBrand.id = 'p' + data.sku
+            productBrand.innerHTML = `${data.make} `;
 
             // let productDiscountPrice = document.querySelector(".info__otherProducts_discount")
-            // productDiscountPrice.setAttribute("data-id", current.id)
-            // productDiscountPrice.id = 'p' + current.id
-            // productDiscountPrice.innerHTML = `${current["discount-price"]}`;
+            // productDiscountPrice.setAttribute("data-id", data.id)
+            // productDiscountPrice.id = 'p' + data.id
+            // productDiscountPrice.innerHTML = `${data["discount-price"]}`;
 
             // FEJL manglelr en if no discount price show normal price only
 
             let productlPrice = document.querySelector(".info__otherProducts_price")
-            productlPrice.setAttribute("data-id", current.sku)
-            productlPrice.id = 'p' + current.sku
-            productlPrice.innerHTML = `${current.price} kr`;
+            productlPrice.setAttribute("data-id", data.sku)
+            productlPrice.id = 'p' + data.sku
+            productlPrice.innerHTML = `${data.price} kr`;
 
             let productText = document.querySelector(".info__produktTekst")
-            productText.setAttribute("data-id", current.sku)
-            productText.id = 'p' + current.sku
-            productText.innerHTML = `${current.description}`;
+            productText.setAttribute("data-id", data.sku)
+            productText.id = 'p' + data.sku
+            productText.innerHTML = `${data.description}`;
 
             // PRODUKT EKSTRA INFORMATION SECTION
             // let info__brand = document.querySelector(".info__brand")
-            // info__brand.setAttribute("data-id", current.id)
-            // info__brand.id = 'p' + current.id
-            // info__brand.innerText = current.description["additional-information"].MANUFACTURER
+            // info__brand.setAttribute("data-id", data.id)
+            // info__brand.id = 'p' + data.id
+            // info__brand.innerText = data.description["additional-information"].MANUFACTURER
 
             // let info__link = document.querySelector(".info__link")
-            // info__link.setAttribute("data-id", current.id)
-            // info__link.id = 'p' + current.id
-            // info__link.innerText = current.description["additional-information"]["MANUFACTURER LINK"]
+            // info__link.setAttribute("data-id", data.id)
+            // info__link.id = 'p' + data.id
+            // info__link.innerText = data.description["additional-information"]["MANUFACTURER LINK"]
 
             // let info__warranty = document.querySelector(".info__warranty")
-            // info__warranty.setAttribute("data-id", current.id)
-            // info__warranty.id = 'p' + current.id
-            // info__warranty.innerText = current.description["additional-information"]["FREE WARRIANTY"]
+            // info__warranty.setAttribute("data-id", data.id)
+            // info__warranty.id = 'p' + data.id
+            // info__warranty.innerText = data.description["additional-information"]["FREE WARRIANTY"]
 
             // let info__charge = document.querySelector(".info__charge")
-            // info__charge.setAttribute("data-id", current.id)
-            // info__charge.id = 'p' + current.id
-            // info__charge.innerText = current.description["additional-information"]["DELIVERY CHARGE"]
+            // info__charge.setAttribute("data-id", data.id)
+            // info__charge.id = 'p' + data.id
+            // info__charge.innerText = data.description["additional-information"]["DELIVERY CHARGE"]
 
             // let info__time = document.querySelector(".info__time")
-            // info__time.setAttribute("data-id", current.id)
-            // info__time.id = 'p' + current.id
-            // info__time.innerText = current.description["additional-information"]["DELIVERY TIME"]
+            // info__time.setAttribute("data-id", data.id)
+            // info__time.id = 'p' + data.id
+            // info__time.innerText = data.description["additional-information"]["DELIVERY TIME"]
 
             // let info__card = document.querySelector(".info__card")
-            // info__card.setAttribute("data-id", current.id)
-            // info__card.id = 'p' + current.id
-            // info__card.innerText = current.description["additional-information"]["CARD SURCHARGES"]
+            // info__card.setAttribute("data-id", data.id)
+            // info__card.id = 'p' + data.id
+            // info__card.innerText = data.description["additional-information"]["CARD SURCHARGES"]
         });
 });
--- tasks/js.js
+++ tasks/js.js
@@ -1,28 +1,40 @@
 
 const gulp = require('gulp');
 const connect = require('gulp-connect');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
+// const sourcemaps = require('gulp-sourcemaps');
+// const babel = require('gulp-babel');
 
 
 function js() {
     return gulp.src('src/js/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('tmp/assets/js'))
         .pipe(connect.reload());
 }
 
+
+// function js() {
+//     return gulp.src('src/js/**/*.js')
+//         .pipe(sourcemaps.init())
+//         .pipe(babel({
+//             presets: ['@babel/env']
+//         }))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest('tmp/assets/js'))
+//         .pipe(connect.reload());
+// }
+
 function buildJS() {
     return gulp.src('src/js/**/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
         .pipe(gulp.dest('dist/assets/js'));
 }
+
+// function buildJS() {
+//     return gulp.src('src/js/**/*.js')
+//         .pipe(babel({
+//             presets: ['@babel/env']
+//         }))
+//         .pipe(gulp.dest('dist/assets/js'));
+// }
 
 function watchJS() {
     return gulp
